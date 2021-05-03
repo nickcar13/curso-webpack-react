3ee9e764c553bced9a918aa94178ef69f6af56ad
Nico Castañeda <nickcar13@gmail.com>
2021-05-03 12:48:14 -0500

    :heavy_plus_sign: adding babel and webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3006,
+    open: true,
+  },
+};
